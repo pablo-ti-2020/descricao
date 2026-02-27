0e1d7407a823d32203d7410b210002b48a332baa
pablo-ti-2020 <pablolabirinto@gmail.com>
2026-02-27 11:21:47 -0300

    Update edificacoes.js

---
+++ materias/edificacoes.js
@@ -0,0 +1,47 @@
+// lista de matérias do curso técnico em Edificações
+
+const materiasEdificacoes = [
+
+"Desenho Técnico para Projetos de Construção Civil",
+
+"Fundamentos de Topografia",
+
+"Introdução a Projetos de Edificações",
+
+"Introdução à Mecânica dos Solos",
+
+"Viabilidade Técnica e Legal de Projetos de Edificações",
+
+"Processos de Construção de Edificações",
+
+"Projetos Arquitetônicos",
+
+"Projetos Estruturais",
+
+"Projetos de Instalações Elétricas",
+
+"Projetos de Instalações Hidrossanitárias",
+
+"Projetos Executivos",
+
+"Gestão de Equipes em Canteiro de Obras",
+
+"Gestão da Manutenção de Edificações",
+
+"Gestão de Máquinas, Equipamentos e Instalações em Obras de Edificações",
+
+"Desempenho de Edificações",
+
+"Planejamento da Construção de Edificações",
+
+"Gestão da Execução de Instalações em Edificações",
+
+"Gestão da Construção de Estruturas em Edificações",
+
+"Gestão da Execução de Acabamentos em Obras de Edificações",
+
+"Métodos Modernos de Construção",
+
+"Inovações Tecnológicas em Projetos de Edificações"
+
+];
